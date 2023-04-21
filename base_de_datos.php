ffcbbf7beb675f1e9c9bb6f4e891a722495ef2e3
AndresCotrina <cotrinandres@gmail.com>
2023-04-21 09:43:36 -0500

    arreglo base de datos

--- base_de_datos.php
+++ base_de_datos.php
@@ -7,7 +7,7 @@ $rutaServidor = "postgresql://postgres:u05QR0zv9cxDJdfCn5ug@containers-us-west-7
 $puerto = "6399";
 
 try{
-    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$port;dbname=$nombreBaseDeDatos",$usuario,$contraseña);
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos",$usuario,$contraseña);
     $base_de_datos -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }catch (Exception $e){
     echo 'Ocurrio un error con la base de datos: ' . $e->getMessage();
